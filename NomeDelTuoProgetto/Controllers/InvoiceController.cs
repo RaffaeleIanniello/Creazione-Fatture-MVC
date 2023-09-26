353067f0f2c04674e2a870b6c7f0854fc165023b
Raffaele Ianniello <134380099+RaffaeleIanniello@users.noreply.github.com>
2023-09-26 12:35:02 +0200

    Aggiungere i file di progetto.

---
+++ NomeDelTuoProgetto/Controllers/InvoiceController.cs
@@ -0,0 +1,47 @@
+﻿using NomeDelTuoProgetto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NomeDelTuoProgetto.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        
+        public static List<Invoice> invoices = new List<Invoice>();
+
+        public ActionResult Index()
+        {
+           
+            decimal totalAmount = 0;
+            foreach (var invoice in invoices)
+            {
+                totalAmount += invoice.Importo;
+            }
+            ViewBag.TotalAmount = totalAmount;
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Invoice invoice)
+        {
+            
+            invoices.Add(invoice);
+
+            return RedirectToAction("PartialView");
+        }
+
+        public new ActionResult PartialView()
+        {
+            return View(invoices);
+        }
+    }
+}
---
+++ NomeDelTuoProgetto/Models/Invoice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NomeDelTuoProgetto.Models
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public string NumeroFattura { get; set; }
+        public DateTime DataEmissione { get; set; }
+        public decimal Importo { get; set; }
+    }
+}
